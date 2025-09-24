bd3e6a961e17a0a6dca18d6777c4cad45c56628b
Barry Dabrowski <dabrowski.barry@gmail.com>
2025-09-24 14:25:23 +0200

    Fix workflows folder name and changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World! I hope its works!");
 
 app.Run();
